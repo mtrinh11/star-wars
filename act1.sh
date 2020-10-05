ae4464e069eac326ef1eef2a4474af624ff1035f
mtrinhwincomp <mtrinh11@gmail.com>
2020-10-05 16:34:08 -0700

    finished problems 1-3

--- act1.sh
+++ act1.sh
@@ -1,9 +1,11 @@
 # 1. In THIS directory, create a new directory called star_wars. > Example answer: mkdir star_wars
 mkdir star_wars
 # 2. In two commands, in the star_wars folder, create two new directories: empire and rebellion. IN THAT ORDER!!!
-
+mkdir star_wars/empire
+mkdir star_wars/rebellion
 
 # 3. Inside the empire directory, create a file called darth_vader.
+touch star_wars/empire/darth_vader
 
 # 4. Use the force (or your echo) to add the text ...heavy breathing... to the darth_vader file. (Don’t remember how to do this? Use the ‘other force’, known as Google!)
 
