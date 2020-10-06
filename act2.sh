4c4a661ce6a3ff6224e3df738b95444ef21b138e
mtrinhwincomp <mtrinh11@gmail.com>
2020-10-05 17:56:57 -0700

    completely all 3 acts, but did not clear tests, checked with classmates, might be a windows issue

--- act2.sh
+++ act2.sh
@@ -1,6 +1,5 @@
-# 1. Inside the `star_wars/rebellion` directory, [IN ONE COMMAND!!] create a file called `princess_leia` with the text 
-# `Help me, Obi-Wan…You’re my only hope.`
-echo 'Help me, Obi-Wan…You’re my only hope.' >> star_wars/rebellion/princess_leia
+# 1. Inside the `star_wars/rebellion` directory, [IN ONE COMMAND!!] create a file called `princess_leia` with the text `Help me, Obi-Wan…You’re my only hope.`
+echo "Help me, Obi-Wan…You’re my only hope." > star_wars/rebellion/princess_leia
 # 2. Create a file called obi_wan in star_wars/rebellion.
 touch star_wars/rebellion/obi_wan
 # 3. Create a file in star_wars/rebellion called luke_skywalker.
@@ -15,4 +14,4 @@ mv star_wars/rebellion/luke_skywalker star_wars/rebellion/millenium_falcon
 mv star_wars/rebellion/obi_wan star_wars/rebellion/millenium_falcon
 mv star_wars/rebellion/princess_leia star_wars/rebellion/millenium_falcon
 # 7. Move the millenium_falcon into the death_star.
-mv star_wars/rebellion/millenium_falcon star_wars/empire/death_star
+mv star_wars/rebellion/millenium_falcon star_wars/empire/death_star 
