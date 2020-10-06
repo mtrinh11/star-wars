ce368d1b3d9a5467473594874125c3a6d298cca4
mtrinhwincomp <mtrinh11@gmail.com>
2020-10-05 18:24:17 -0700

    fixed some answers in act3.sh

--- act3.sh
+++ act3.sh
@@ -8,10 +8,10 @@ rm star_wars/empire/death_star/obi_wan
 mv star_wars/empire/death_star/luke_skywalker star_wars/empire/death_star/millenium_falcon
 mv star_wars/empire/death_star/princess_leia star_wars/empire/death_star/millenium_falcon
 mv star_wars/empire/death_star/han_solo star_wars/empire/death_star/millenium_falcon
-mv star_wars/empire/death_star/ chewy star_wars/empire/death_star/millenium_falcon
+mv star_wars/empire/death_star/chewy star_wars/empire/death_star/millenium_falcon
 # 4. Move the millenium_falcon back into the rebellion directory.
 mv star_wars/empire/death_star/millenium_falcon star_wars/rebellion
 # 5. darth_vader leaves the death_star to pursue Luke! Move him from the death_star into the empire directory!
 mv star_wars/empire/death_star/darth_vader star_wars/empire
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
-rm -r star_wars/empire/death_star 
+rm -r star_wars/empire/death_star
